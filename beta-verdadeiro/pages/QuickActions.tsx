9531d728337430314269521b6af0caad463ee155
murillinho <124073661+murillinho@users.noreply.github.com>
2025-11-24 11:19:49 -0300

    Delete beta-verdadeiro directory

--- beta-verdadeiro/pages/QuickActions.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Componente limpo conforme solicitado (aba removida)
-const QuickActions: React.FC = () => {
-  return null;
-};
-
-export default QuickActions;
--- beta-verdadeiro/vite.config.ts
+++
@@ -1,1 +0,0 @@
-// File deprecated. Switched to Parcel.
